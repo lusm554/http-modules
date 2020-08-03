c3f278c887e4f907de7b5d130021500022901a3b
loveyousomuch <loveyousomuch554@gmail.com>
2020-08-04 01:34:31 +0300

    control via npm

---
+++ check_request/client-delete.js
@@ -0,0 +1,25 @@
+const http = require('http')
+
+const someData = {name: 'Milk', id: 4}
+
+const justJsonData = JSON.stringify(someData)
+
+const options = {
+    hostname: '127.0.0.1',
+    port: 3000,
+    path: `/products/${someData.id}`,
+    method: 'DELETE',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': justJsonData.length
+    }
+}
+
+const request = http.request(options, (res) => {
+    let body = ''
+    res.on('data', (chunk) => { body+= chunk })
+    res.on('end', () => { console.log('respons', body) })
+    res.on('close', () => { console.log('connection close') })
+})
+
+request.end(justJsonData)
